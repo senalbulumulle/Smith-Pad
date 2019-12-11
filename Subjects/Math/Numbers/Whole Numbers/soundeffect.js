5762d4bf3dfcc2d65ca226d0b280460ea279ea5e
Senal Bulumulle <senal.bulumulle@gmail.com>
2019-12-11 11:34:36 -0500

    Made files on Subjects/Math/Numbers./Whole%20Numbers/ That talks about Place Values.
    Made files on Subjects/Math/Numbers/Whole%20Numbers/. That talks about Place Values.

---
+++ Subjects/Math/Numbers/Whole Numbers/soundeffect.js
@@ -0,0 +1,44 @@
+// FOIL Bar Sound Effects 
+
+/* 
+
+
+Written by Senal Bulumulle
+
+/*
+
+
+Call Syntaxes in HTML 
+onmousedown="buttonPress.play()"
+onmouseup="buttonPress.play()"
+onload="buttonPress.play()"
+
+
+*/
+/***************************************************************** 
+                    Variables 
+/***************************************************************** */
+
+var buttonPress = new Audio();
+var hoverPress = new Audio();
+
+/*****************************************************************
+                        Source Files
+
+* This Can be Consisted from Local File or HTTPS File
+/***************************************************************** */
+
+
+/***************************************************************** */
+
+buttonPress.src = './Smith_Pad_Button.mp3';
+
+
+
+/***************************************************************** */
+
+
+/***************************************************************** 
+                        Console.log 
+
+/***************************************************************** */
