9fa9efaaea02e9a8fb971fa712d666bf82b81b8b
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-01-15 08:36:56 -0500

    Added some major files/changes to interact with the Raspberry Pi Hardware.
    Added some major files/changes to interact with the Raspberry Pi Hardware.

---
+++ Power/reboot.php
@@ -0,0 +1,60 @@
+<?php
+exec("reboot");
+?>
+
+<link rel="stylesheet" href="../src/Custom_Tags.css" type="text/css">
+<link rel="stylesheet" href="../src/position-text.css" type="text/css">
+<link rel="stylesheet" href="../src/UI.css" type="text/css">
+<link rel="stylesheet" href="../src/FOIL-Bar.css" type="text/css">
+<style>
+
+    body{
+        background-color:black;
+        color:white;
+    }
+
+
+</style>
+
+<center>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+
+
+<fontsize90> Rebooting.... </fontsize90> 
+</center>
---
+++ Power/shutdown.php
@@ -0,0 +1,66 @@
+<!--
+<?php
+exec("shutdown");
+?>-->
+
+<link rel="stylesheet" href="../src/Custom_Tags.css" type="text/css">
+<link rel="stylesheet" href="../src/position-text.css" type="text/css">
+<link rel="stylesheet" href="../src/UI.css" type="text/css">
+<link rel="stylesheet" href="../src/FOIL-Bar.css" type="text/css">
+
+<style>
+
+    body{
+        background-color:black;
+        color:white;
+    }
+
+
+</style>
+
+
+
+<center>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+    <div class="space"></div>
+
+
+<fontsize90> Shutting Down.... </fontsize90> 
+<ul></ul>
+<fontsize40> In a Minute</fontsize40>
+</center>
