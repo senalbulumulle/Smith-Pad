007cac84f9d8bc790158985bce76b3ad1421dd72
Senal Bulumulle <senal.bulumulle@gmail.com>
2020-02-01 16:05:08 -0500

    Made the CD command to go to /Home/pi to find the Update File
    Made the CD command to go to /Home/pi to find the Update File

--- Update-Tool/update.sh
+++ Update-Tool/update.sh
@@ -7,7 +7,7 @@
 #   Version 1.0 
 ###########################################################################
 ## Change Directory 
-cd 
+cd /Home/pi
 ## Find the Update file 
 sh update.sh
 ## Once the update.sh file is found it will run the software update. 
