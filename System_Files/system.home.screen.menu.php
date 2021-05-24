115744852d2b9231153f0e618097efaea51a7787
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-05-24 16:48:55 -0400

    Added some menu buttons in Applicatins menu
    This file is located in Smith-Pad/System_Files/system.home.screen.menu.php

--- System_Files/system.home.screen.menu.php
+++ System_Files/system.home.screen.menu.php
@@ -20,6 +20,21 @@
 <a class="applicationsclose" href="#">&times;</a>
 
 
+<!--################################## LINUX / KDE BASED APPLICATIONS  #############################-->
+<a href="#chromium" href="#" class="button color-is-red">Chromium</a>
+<a href="#chromium" href="#" class="button color-is-blue">Dolphin File Manager</a>
+<a href="#chromium" href="#" class="button color-is-blue">Konsole Terminal</a>
+<!--#########################################################################################-->
+
+
+<?php include "./System_Files/system.global.space.5.php";?>
+
+
+
+<!--################################## CUSTOM WEB LINK  #############################-->
+<a href="https://launch.ocps.net" href="#" class="button color-is-blue">OCPS Classlink LaunchPad</a>
+<a href="https://edpuzzle.com" class="button color-is-blue">Edpuzzle</a>
+<!--################################ CHANGE ON PRODUCTION ###########################-->
 </div>
 </div>
 </div>
