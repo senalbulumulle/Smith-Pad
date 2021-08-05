fd027debc37d468161247e618c15c60ee0c89c4c
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-08-05 14:41:14 -0400

    Fixed the issue when the Konsole terminal opens via Smith-Pad-OS-Interface (Part-1)

--- System_Files/system.app.launcher.external.local.app.name.konsole.terminal.php
+++ System_Files/system.app.launcher.external.local.app.name.konsole.terminal.php
@@ -2,6 +2,17 @@
 
 
 <?php
-$launch = shell_exec('konsole');
+
+
+// The first step is to call shell_exec to change the current directory to the 
+// default Home Directory, that way the KDE Application, Konsole will not 
+// open with the directory that shows the System_Files on the Smith-Pad-OS-Interface 
+// Source Code. It makes it easier, instead of the User to manually change the directory
+// to the default directory by putting 'cd', which does to the default home directory, 
+// unless the default directory has been specified by the administrator or by the user's 
+// preferences. Please see the "system.app.launcher.external.app.name"
+
+$launch = shell_exec('cd');
 echo "<pre>$launch</pre>";
+
 ?>
